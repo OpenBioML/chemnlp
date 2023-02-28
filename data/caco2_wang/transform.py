03da9f2c8bd2537afedd947a83fa343f9b0603ac
Michael Pieler <Michael.Pieler@Gmail.com>
2023-02-28 16:47:19 +0100

    feat: fix black lint

--- data/caco2_wang/transform.py
+++ data/caco2_wang/transform.py
@@ -109,7 +109,8 @@ def get_and_transform_data():
 
     def str_presenter(dumper, data):
         """configures yaml for dumping multiline strings
-        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data"""
+        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+        """
         if data.count("\n") > 0:  # check for multiline string
             return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
         return dumper.represent_scalar("tag:yaml.org,2002:str", data)
