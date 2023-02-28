e28a854b16682238790687ffb2b30af757a470b4
Michael Pieler <Michael.Pieler@Gmail.com>
2023-02-28 16:30:59 +0100

    feat: update nice multiline string yaml saving

--- data/caco2_wang/transform.py
+++ data/caco2_wang/transform.py
@@ -106,6 +106,18 @@ def get_and_transform_data():
             }""",
         ],
     }
+
+    def str_presenter(dumper, data):
+        """configures yaml for dumping multiline strings
+        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data"""
+        if data.count("\n") > 0:  # check for multiline string
+            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+
+    yaml.add_representer(str, str_presenter)
+    yaml.representer.SafeRepresenter.add_representer(
+        str, str_presenter
+    )  # to use with safe_dum
     fn_meta = "meta.yaml"
     with open(fn_meta, "w") as f:
         yaml.dump(meta, f, sort_keys=False)
