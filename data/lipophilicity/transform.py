de3602350a2393a9469d9dad949dd6fbdc6d0a47
Michael Pieler <Michael.Pieler@Gmail.com>
2023-03-01 12:51:13 +0100

    feat: address review

--- data/lipophilicity/transform.py
+++ data/lipophilicity/transform.py
@@ -60,10 +60,6 @@ def get_and_transform_data():
         "license": "CC BY-SA 3.0",  # license under which the original dataset was published
         "links": [  # list of relevant links (original dataset, other uses, etc.)
             {
-                "url": "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/Lipophilicity.csv",
-                "description": "original dataset",
-            },
-            {
                 "url": "https://github.com/cheminfo/molecule-features/blob/main/data/lipophilicity/meta.yaml",
                 "description": "original meta data",
             },
@@ -91,9 +87,23 @@ def get_and_transform_data():
             title={ChEMBL Deposited Data Set-AZ dataset},
             author={Hersey, Anne},
             year={2015},
-            institution={Technical Report, Technical report, EMBL-EBI, 2015. https://www. ebi. ac. uk~…}}"""
+            institution={Technical Report, Technical report, EMBL-EBI, 2015. https://www. ebi. ac. uk}
+            }"""
         ],
     }
+
+    def str_presenter(dumper, data):
+        """configures yaml for dumping multiline strings
+        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+        """
+        if data.count("\n") > 0:  # check for multiline string
+            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+
+    yaml.add_representer(str, str_presenter)
+    yaml.representer.SafeRepresenter.add_representer(
+        str, str_presenter
+    )  # to use with safe_dum
     fn_meta = "meta.yaml"
     with open(fn_meta, "w") as f:
         yaml.dump(meta, f, sort_keys=False)
