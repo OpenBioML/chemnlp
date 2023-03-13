c9341ffa7e9dfaada9c90e9eff0125df59617255
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-03-13 09:23:14 +0100

    chore: continue cleanup

--- data/tyrosyl-dna_phosphodiesterase_butkiewicz/transform.py
+++ data/tyrosyl-dna_phosphodiesterase_butkiewicz/transform.py
@@ -6,15 +6,15 @@ from tdc.single_pred import HTS
 def get_and_transform_data():
     # get raw data
     label = "tyrosyl-dna_phosphodiesterase_butkiewicz"
-    data = HTS(name=label)
-    fn_data_original = "data_original.csv"
-    data.get_data().to_csv(fn_data_original, index=False)
+    splits = HTS(name=label).get_split()
+    df_train = splits["train"]
+    df_valid = splits["valid"]
+    df_test = splits["test"]
+    df_train["split"] = "train"
+    df_valid["split"] = "valid"
+    df_test["split"] = "test"
 
-    # create dataframe
-    df = pd.read_csv(
-        fn_data_original,
-        delimiter=",",
-    )  # not necessary but ensure we can load the saved data
+    df = pd.concat([df_train, df_valid, df_test], axis=0)
 
     # check if fields are the same
     fields_orig = df.columns.tolist()
@@ -22,6 +22,7 @@ def get_and_transform_data():
         "Drug_ID",
         "Drug",
         "Y",
+        "split",
     ]
 
     # overwrite column names = fields
@@ -29,6 +30,7 @@ def get_and_transform_data():
         "compound_id",
         "SMILES",
         "activity_tyrosyl_dna_phosphodiesterase",
+        "split",
     ]
     df.columns = fields_clean
 
@@ -41,30 +43,23 @@ def get_and_transform_data():
     # create meta yaml
     meta = {
         "name": "tyrosyl-dna_phosphodiesterase_butkiewicz",  # unique identifier, we will also use this for directory names
-        "description": """These are nine high-quality high-throughput screening (HTS) datasets from [1]. \
-        These datasets were curated from HTS data at the PubChem database [2]. \
-        Typically, HTS categorizes small molecules into hit, inactive, or unspecified against a certain therapeutic target. \
-        However, a compound may be falsely classified as a hit due to experimental artifacts such as optical interference. \
-        Moreover, because the screening is performed without duplicates, \
-        and the cutoff is often set loose to minimize the false negative rates, \
-        the results from the primary screens often contain high false positive rates [3]. \
-        Hence the result from the primary screen is only used as the first iteration to reduce the compound library \
-        to a smaller set of further confirmatory tests. Here each dataset is carefully collated through confirmation screens \
-        to validate active compounds. The curation process is documented in [1]. Each dataset is identified by the PubChem Assay ID (AID). \
-        Features of the datasets: (1) At least 150 confirmed active compounds present; \
-        (2) Diverse target classes; (3) Realistic (large number and highly imbalanced label).""",
+        "description": """Inhibition of Human tyrosyl-DNA phosphodiesterase 1 (TDP1) \
+            potentially enhances anticancer activity of DNA topoisomerase I inhibitors.
+            Primary screen AID 485290. Counter screen AID 489007.\
+            Final set contains all compounds active in the counter screen AID 489007.
+            """,
         "targets": [
             {
-                "id": "activity_tyrosyl_dna_phosphodiesterase",  # name of the column in a tabular dataset
-                "description": "whether it active against tyrosyl-dna phosphodiesterase receptor (1) or not (0).",  # description of what this column means
-                "units": "activity",  # units of the values in this column (leave empty if unitless)
-                "type": "categorical",  # can be "categorical", "ordinal", "continuous"
-                "names": [  # names for the property (to sample from for building the prompts)
-                    "tyrosyl-dna phosphodiesterase activity",
-                    "tyrosyl-dna phosphodiesterase Inhibitor",
-                    "activity against tyrosyl-dna phosphodiesterase",
-                    "tyrosyl-dna phosphodiesterase receptor",
+                "id": "activity_tyrosyl_dna_phosphodiesterase",
+                "description": "whether it active against tyrosyl-dna phosphodiesterase receptor (1) or not (0).",
+                "units": None,
+                "type": "boolean",
+                "names": [
+                    "inhibiting human tyrosyl-DNA phosphodiesterase 1 (TDP1)",
+                    "a tyrosyl-DNA phosphodiesterase 1 (TDP1) inhibitor",
                 ],
+                "pubchem_aids": [485290, 489007],
+                "uris": [],
             },
         ],
         "identifiers": [
@@ -101,8 +96,10 @@ def get_and_transform_data():
               volume = {18},
               number = {1},
               pages = {735--756},
-              author = {Mariusz Butkiewicz and Edward Lowe and Ralf Mueller and Jeffrey Mendenhall and Pedro Teixeira and C. Weaver and Jens Meiler},
-              title = {Benchmarking Ligand-Based Virtual High-Throughput Screening with the {PubChem} Database},
+              author = {Mariusz Butkiewicz and Edward Lowe and Ralf Mueller \
+              and Jeffrey Mendenhall and Pedro Teixeira and C. Weaver and Jens Meiler},
+              title = {Benchmarking Ligand-Based Virtual High-Throughput Screening \
+                with the {PubChem} Database},
               journal = {Molecules}}""",
             """@article{Kim2018,
               doi = {10.1093/nar/gky1033},
@@ -113,7 +110,9 @@ def get_and_transform_data():
               volume = {47},
               number = {D1},
               pages = {D1102--D1109},
-              author = {Sunghwan Kim and Jie Chen and Tiejun Cheng and Asta Gindulyte and Jia He and Siqian He and Qingliang Li and Benjamin A Shoemaker and Paul A Thiessen and Bo Yu and Leonid Zaslavsky and Jian Zhang and Evan E Bolton},
+              author = {Sunghwan Kim and Jie Chen and Tiejun Cheng and Asta Gindulyte \
+                and Jia He and Siqian He and Qingliang Li and Benjamin A Shoemaker \
+                    and Paul A Thiessen and Bo Yu and Leonid Zaslavsky and Jian Zhang and Evan E Bolton},
               title = {{PubChem} 2019 update: improved access to chemical data},
               journal = {Nucleic Acids Research}}""",
             """@article{Butkiewicz2017,
@@ -123,15 +122,18 @@ def get_and_transform_data():
               publisher = {Chem Inform},
               volume = {3},
               number = {1},
-              author = {Butkiewicz, M.  and Wang, Y.  and Bryant, S. H.  and Lowe, E. W.  and Weaver, D. C.  and Meiler, J.},
-              title = {{H}igh-{T}hroughput {S}creening {A}ssay {D}atasets from the {P}ub{C}hem {D}atabase}},
+              author = {Butkiewicz, M.  and Wang, Y.  and Bryant, S. H.  \
+                and Lowe, E. W.  and Weaver, D. C.  and Meiler, J.},
+              title = {{H}igh-{T}hroughput {S}creening {A}ssay {D}atasets \
+                from the {P}ub{C}hem {D}atabase}},
               journal = {Chemical Science}}""",
         ],
     }
 
     def str_presenter(dumper, data):
         """configures yaml for dumping multiline strings
-        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+        Ref:
+        https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
         """
         if data.count("\n") > 0:  # check for multiline string
             return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
