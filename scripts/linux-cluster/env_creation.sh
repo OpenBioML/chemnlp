57f0c1c77e8e4ce0c745be8133429906965b057b
jack.butler <jack.butler@faculty.ai>
2023-03-14 14:18:52 +0000

    fix: update paths used in env script

--- scripts/linux-cluster/env_creation.sh
+++ scripts/linux-cluster/env_creation.sh
@@ -1,18 +1,19 @@
 #! /bin/bash
-# Must already have miniconda installed!
-
-cd ~
-
-# Create Python env
-export CONDA_ENV_PATH=/fsx/conda/env/chemnlp-standard
+## Must already have miniconda installed!
+export CONDA_ENV_PATH=/fsx/proj-chemnlp/conda/env/chemnlp-standard
 export PYTHON_VER=3.8
+
+# Create Python environment through conda
 conda create --prefix ${CONDA_ENV_PATH} python=${PYTHON_VER} -y
 conda activate ${CONDA_ENV_PATH}
 
 # Python requirements
+## cd into your directory inside of proj-chemnlp
+cd /fsx/proj-chemnlp/${USER}
+
 ## clone + submodules
 git clone --recurse-submodules --remote-submodules git@github.com:OpenBioML/chemnlp.git
 
 ## install
 pip install chemnlp # our repo
-pip install -r chemnlp/gpt-neox/requirements/requirements.txt # basic gpt-neox requirements
+pip install -r chemnlp/gpt-neox/requirements/requirements.txt # base gpt-neox requirements
