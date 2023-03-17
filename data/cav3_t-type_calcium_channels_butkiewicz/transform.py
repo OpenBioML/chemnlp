abd324e5dd3bcf882f5519d78cc80d2775f28c8b
Michael Pieler <Michael.Pieler@Gmail.com>
2023-03-17 16:51:36 +0100

    feat: cleanup cav3_t-type_calcium_channels_butkiewicz

--- data/cav3_t-type_calcium_channels_butkiewicz/transform.py
+++ data/cav3_t-type_calcium_channels_butkiewicz/transform.py
@@ -38,14 +38,12 @@ def get_and_transform_data():
     # create meta yaml
     meta = {
         "name": "cav3_t-type_calcium_channels_butkiewicz",
-        "description": """
-        This dataset was initially curated from HTS data at the PubChem database. \
-        The curation process is documented in Butkiewicz et al. \
-        Primary screening with AID 449739 identified inhibitors of Cav3 T-type calcium channels. \
-        Four follow-up screens were performed to confirm inhibitory effects on smaller sets of compounds \
-        involving AID 493021, AID 493022, AID 493023, and AID 493041.
-        AID 489005 was performed as counter screen validating active compounds of the primary screen. \
-        """,
+        "description": """This dataset was initially curated from HTS data at the PubChem database.
+The curation process is documented in Butkiewicz et al.
+Primary screening with AID 449739 identified inhibitors of Cav3 T-type calcium channels.
+Four follow-up screens were performed to confirm inhibitory effects on smaller sets of compounds
+involving AID 493021, AID 493022, AID 493023, and AID 493041.
+AID 489005 was performed as counter screen validating active compounds of the primary screen.""",
         "targets": [
             {
                 "id": "activity_cav3_t_type_calcium_channels",  # name of the column in a tabular dataset
@@ -88,43 +86,43 @@ def get_and_transform_data():
         "url": "https://tdcommons.ai/single_pred_tasks/hts/#butkiewicz-et-al",
         "bibtex": [
             """@article{Butkiewicz2013,
-              doi = {10.3390/molecules18010735},
-              url = {https://doi.org/10.3390/molecules18010735},
-              year = {2013},
-              month = jan,
-              publisher = {{MDPI} {AG}},
-              volume = {18},
-              number = {1},
-              pages = {735--756},
-              author = {Mariusz Butkiewicz and Edward Lowe and Ralf Mueller and Jeffrey Mendenhall \
-                and Pedro Teixeira and C. Weaver and Jens Meiler},
-              title = {Benchmarking Ligand-Based Virtual High-Throughput Screening with the {PubChem} Database},
-              journal = {Molecules}}""",
+doi = {10.3390/molecules18010735},
+url = {https://doi.org/10.3390/molecules18010735},
+year = {2013},
+month = jan,
+publisher = {{MDPI} {AG}},
+volume = {18},
+number = {1},
+pages = {735--756},
+author = {Mariusz Butkiewicz and Edward Lowe and Ralf Mueller and Jeffrey Mendenhall
+and Pedro Teixeira and C. Weaver and Jens Meiler},
+title = {Benchmarking Ligand-Based Virtual High-Throughput Screening with the {PubChem} Database},
+journal = {Molecules}}""",
             """@article{Kim2018,
-              doi = {10.1093/nar/gky1033},
-              url = {https://doi.org/10.1093/nar/gky1033},
-              year = {2018},
-              month = oct,
-              publisher = {Oxford University Press ({OUP})},
-              volume = {47},
-              number = {D1},
-              pages = {D1102--D1109},
-              author = {Sunghwan Kim and Jie Chen and Tiejun Cheng and Asta Gindulyte and Jia He and Siqian He \
-            and Qingliang Li and Benjamin A Shoemaker and Paul A Thiessen and Bo Yu and Leonid Zaslavsky \
-            and Jian Zhang and Evan E Bolton},
-              title = {{PubChem} 2019 update: improved access to chemical data},
-              journal = {Nucleic Acids Research}}""",
+doi = {10.1093/nar/gky1033},
+url = {https://doi.org/10.1093/nar/gky1033},
+year = {2018},
+month = oct,
+publisher = {Oxford University Press ({OUP})},
+volume = {47},
+number = {D1},
+pages = {D1102--D1109},
+author = {Sunghwan Kim and Jie Chen and Tiejun Cheng and Asta Gindulyte and Jia He and Siqian He
+and Qingliang Li and Benjamin A Shoemaker and Paul A Thiessen and Bo Yu and Leonid Zaslavsky
+and Jian Zhang and Evan E Bolton},
+title = {{PubChem} 2019 update: improved access to chemical data},
+journal = {Nucleic Acids Research}}""",
             """@article{Butkiewicz2017,
-              doi = {},
-              url = {https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5962024/},
-              year = {2017},
-              publisher = {Chem Inform},
-              volume = {3},
-              number = {1},
-              author = {Butkiewicz, M.  and Wang, Y.  and Bryant, S. H.  and Lowe, E. W.  and Weaver, D. C.  \
-            and Meiler, J.},
-              title = {{H}igh-{T}hroughput {S}creening {A}ssay {D}atasets from the {P}ub{C}hem {D}atabase}},
-              journal = {Chemical Science}}""",
+doi = {},
+url = {https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5962024/},
+year = {2017},
+publisher = {Chem Inform},
+volume = {3},
+number = {1},
+author = {Butkiewicz, M.  and Wang, Y.  and Bryant, S. H.  and Lowe, E. W.  and Weaver, D. C.
+and Meiler, J.},
+title = {{H}igh-{T}hroughput {S}creening {A}ssay {D}atasets from the {P}ub{C}hem {D}atabase}},
+journal = {Chemical Science}}""",
         ],
     }
 
