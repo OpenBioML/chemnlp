e1299ff5fe935b6ec10e237a3383ee2913438e1e
Michael Pieler <Michael.Pieler@Gmail.com>
2023-03-22 16:42:29 +0100

    feat: benchmark fields cav3_t-type_calcium_channels_butkiewicz

--- data/cav3_t-type_calcium_channels_butkiewicz/transform.py
+++ data/cav3_t-type_calcium_channels_butkiewicz/transform.py
@@ -82,6 +82,13 @@ AID 489005 was performed as counter screen validating active compounds of the pr
             },
         ],
         "split_col": "split",  # name of the column that contains the split information
+        "benchmarks": [
+            {
+                "name": "TDC",
+                "link": "https://tdcommons.ai/",
+                "split_column": "split",
+            },
+        ],
         "num_points": len(df),  # number of datapoints in this dataset
         "url": "https://tdcommons.ai/single_pred_tasks/hts/#butkiewicz-et-al",
         "bibtex": [
