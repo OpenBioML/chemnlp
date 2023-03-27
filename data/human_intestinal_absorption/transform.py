81c60310220b5dd81e0150b2bb8335d24a8daeb0
Mohamed Abd Elaleem <109590482+phalem@users.noreply.github.com>
2023-03-27 19:27:42 +0200

    Add files via upload

---
+++ data/human_intestinal_absorption/transform.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import yaml
+from tdc.single_pred import ADME
+
+def get_and_transform_data():
+    # get raw data
+    target_folder = 'Human_Intestinal_Absorption'
+    target_subfolder = 'HIA_Hou'
+    data = ADME(name = target_subfolder)
+    fn_data_original = "data_original.csv"
+    data.get_data().to_csv(fn_data_original, index=False)
+    
+    # create dataframe
+    df = pd.read_csv(
+        fn_data_original,
+        delimiter=",",
+    )  # not necessary but ensure we can load the saved data
+    df['Drug_ID'] = [x.split('.')[0] for x in df.Drug_ID.tolist()]
+
+    # check if fields are the same
+    fields_orig = df.columns.tolist()
+    assert fields_orig == [
+        "Drug_ID",
+        "Drug",
+        "Y",
+    ]
+
+
+    # overwrite column names = fields
+    fields_clean =['compound_name', 'SMILES', f"absorption_{target_subfolder}"]
+    df.columns = fields_clean
+
+    # data cleaning
+#     df.compound_name = (
+#         df.compound_name.str.strip()
+#     )  
+    # remove leading and trailing white space characters
+    df = df.dropna()
+    assert not df.duplicated().sum()
+    
+    # save to csv
+    fn_data_csv = "data_clean.csv"
+    df.to_csv(fn_data_csv, index=False)
+    
+    # create meta yaml
+    meta =  {
+        "name": "human_intestinal_absorption",  # unique identifier, we will also use this for directory names
+        "description": """When a drug is orally administered, it needs to be absorbed from the
+human gastrointestinal system into the bloodstream of the human body. This ability
+of absorption is called human intestinal absorption (HIA) and it is crucial
+for a drug to be delivered to the target.""",
+        "targets": [
+            {
+                "id": f"absorption_{target_subfolder}",  # name of the column in a tabular dataset
+                "description": "whether it absorption activity of HIA (1) or not active (0)",  # description of what this column means
+                "units": "HIA_absorption",  # units of the values in this column (leave empty if unitless)
+                "type": "categorical",  # can be "categorical", "ordinal", "continuous"
+                "names": [  # names for the property (to sample from for building the prompts)
+                    "Human Intestinal Absorption",
+                    "HIA absorption",
+                    "HIA",
+                    "ADME absorption HIA",
+                    "drug delivery",
+                    "HIA activity",
+                    "HIA absorption activity"
+                ],
+                "uris":[
+                "https://bioportal.bioontology.org/ontologies/MESH?p=classes&conceptid=http%3A%2F%2Fpurl.bioontology.org%2Fontology%2FMESH%2FD007408",
+                ],
+            },
+        ],
+        "benchmarks": [
+        {
+            "name": "TDC",  # unique benchmark name
+            "link": "https://tdcommons.ai/",  # benchmark URL
+            "split_column": "split",  # name of the column that contains the split information
+        },
+            ],
+        "identifiers": [
+             {
+                "id": "SMILES",  # column name
+                "type": "SMILES",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
+                "description": "SMILES",  # description (optional, except for "Other")
+            },
+            {
+                "id": "compound_name",  # column name
+                "type": "Other",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
+                "names":[
+                    "drug bank name",
+                    "drug name pubchem",
+                    "drug generic name",
+                    "drug chemical (generic) name",
+                    "chemical name"
+                ],
+                "description": "Drug name",  # description (optional, except for "Other")
+            },
+        ],
+        "license": "CC BY 4.0",  # license under which the original dataset was published
+        "links": [  # list of relevant links (original dataset, other uses, etc.)
+            {
+                "url": "https://doi.org/10.1021/ci600343x",
+                "description": "corresponding publication",
+            },
+            {
+                "url": "https://tdcommons.ai/single_pred_tasks/adme/#hia-human-intestinal-absorption-hou-et-al",
+                "description": "data source",
+
+            }
+        ],
+        "num_points": len(df),  # number of datapoints in this dataset
+        "bibtex": [
+            """@article{Hou2006,
+doi = {10.1021/ci600343x},
+url = {https://doi.org/10.1021/ci600343x},
+year = {2006},
+month = nov,
+publisher = {American Chemical Society (ACS)},
+volume = {47},
+number = {1},
+pages = {208--218},
+author = {Tingjun Hou and Junmei Wang and Wei Zhang and Xiaojie Xu},
+title = {ADME Evaluation in Drug Discovery. 7. Prediction of Oral Absorption
+by Correlation and Classification},
+journal = {Journal of Chemical Information and Modeling}""",
+        ],
+    }
+    
+    def str_presenter(dumper, data):
+        """configures yaml for dumping multiline strings
+        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+        """
+        if data.count("\n") > 0:  # check for multiline string
+            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+
+    yaml.add_representer(str, str_presenter)
+    yaml.representer.SafeRepresenter.add_representer(
+        str, str_presenter
+    )  # to use with safe_dum
+    fn_meta = "meta.yaml"
+    with open(fn_meta, "w") as f:
+        yaml.dump(meta, f, sort_keys=False)
+
+    print(f"Finished processing {meta['name']} dataset!")
+
+if __name__ == "__main__":
+    get_and_transform_data()
