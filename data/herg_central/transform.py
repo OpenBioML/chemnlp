ef2df98e9d747888805a1ba32d5456ada99899ce
Michael Pieler <Michael.Pieler@Gmail.com>
2023-03-30 15:08:18 +0200

    feat: cleanup

--- data/herg_central/transform.py
+++ data/herg_central/transform.py
@@ -3,14 +3,30 @@ import yaml
 from tdc.single_pred import Tox
 from tdc.utils import retrieve_label_name_list
 
+
 def get_and_transform_data():
     # get raw data
-    label = 'herg_central'
-    df_all = [Tox(name = label, label_name = x).get_data(format = 'df') for x in retrieve_label_name_list(label)]    
-    df_path = f'data/{label}.tab'
-    df = pd.read_csv(df_path, sep = '\t')
+    name = "herg_central"
+    label_names = retrieve_label_name_list("herg_central")
+    dfs = []
+    for ln in label_names:
+        # get raw data
+        splits = Tox(name=name, label_name=ln).get_split()
+        df_train = splits["train"]
+        df_valid = splits["valid"]
+        df_test = splits["test"]
+        df_train["split"] = "train"
+        df_valid["split"] = "valid"
+        df_test["split"] = "test"
+        df = pd.concat([df_train, df_valid, df_test], axis=0)
+        df["task"] = ln
+        dfs.append(df)
+
+    df = pd.concat(dfs, axis=0)
+
     fn_data_original = "data_original.csv"
-    df.to_csv(fn_data_original, index=None)
+    df.to_csv(fn_data_original, index=False)
+    del df, dfs, df_train, df_valid, df_test
 
     # create dataframe
     df = pd.read_csv(
@@ -20,27 +36,11 @@ def get_and_transform_data():
 
     # check if fields are the same
     fields_orig = df.columns.tolist()
-    assert fields_orig == [
-        'ID',
-        'X', 
-        'hERG_at_1uM',
-        'hERG_at_10uM', 
-        'hERG_inhib']
+    assert fields_orig == ["Drug_ID", "Drug", "Y", "split", "task"]
 
     # overwrite column names = fields
-    fields_clean =  [
-        "compound_id",
-        "SMILES",
-        "hERG_at_1uM",
-        "hERG_at_10uM",
-        "hERG_inhib",
-    ]
+    fields_clean = ["compound_id", "SMILES", "result", "split", "task"]
     df.columns = fields_clean
-
-#     # data cleaning
-#     df.compound_name = (
-#         df.compound_name.str.strip()
-#     )  # remove leading and trailing white space characters
 
     assert not df.duplicated().sum()
 
@@ -50,96 +50,85 @@ def get_and_transform_data():
 
     # create meta yaml
     meta = {
-            "name": "herg_central",  # unique identifier, we will also use this for directory names
-            "description": """Human ether-a-go-go related gene (hERG) is crucial for the coordination
+        "name": "herg_central",  # unique identifier, we will also use this for directory names
+        "description": """Human ether-a-go-go related gene (hERG) is crucial for the coordination
 of the heart's beating. Thus, if a drug blocks the hERG, it could lead to severe
 adverse effects. Therefore, reliable prediction of hERG liability in the early
 stages of drug design is quite important to reduce the risk of cardiotoxicity-related
 attritions in the later development stages. There are three targets: hERG_at_1microM,
 hERG_at_10microM, and hERG_inhib.""",
-            "targets": [
-                {
-                    "id": "hERG_at_1uM",  # name of the column in a tabular dataset
-                    "description": "The percent inhibition at a 1microM concentration",  # description of what this column means
-                    "units": "1microM concentration",  # units of the values in this column (leave empty if unitless)
-                    "type": "continuous",  # can be "categorical", "ordinal", "continuous"
-                    "names": [  # names for the property (to sample from for building the prompts)
-                        "hERG activity",
-                        "hERG active compound",
-                        "hERG blocker",
-                        "Human ether-a-go-go related gene (hERG) blocker",
-                        "Activity against Human ether-a-go-go related gene (hERG)",
-                        "hERG at 1microM",
-                        "hERG activity 1microM",
-                        "The percent inhibition at a 1microM concentration",
-                        "Compound percent activity at 1microM"
-                        ,
-                    ],
-                "uris":[
-                "https://bioportal.bioontology.org/ontologies/MI?p=classes&conceptid=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FMI_2136",
+        "targets": [
+            {
+                "id": "hERG_at_1uM",
+                "description": "the percent inhibition of hERG at a 1uM concentration",
+                "units": "uM",
+                "type": "continuous",
+                "names": [
+                    "hERG percent inhibition at a 1uM concentration",
+                    "hERG percent inhibition at 1uM",
+                    "Human ether-a-go-go related gene (hERG) percent inhibition at a 1uM concentration",
+                    "Human ether-a-go-go related gene (hERG) percent inhibition at 1uM",
+                ],
+                "uris": [
+                    "http://purl.obolibrary.org/obo/MI_2136",
+                ],
+            },
+            {
+                "id": "hERG_at_10uM",
+                "description": "the percent inhibition of hERG at a 10uM concentration",
+                "units": "uM",
+                "type": "continuous",
+                "names": [
+                    "hERG percent inhibition at a 10uM concentration",
+                    "hERG percent inhibition at 10uM",
+                    "Human ether-a-go-go related gene (hERG) percent inhibition at a 10uM concentration",
+                    "Human ether-a-go-go related gene (hERG) percent inhibition at 10uM",
+                ],
+                "uris": ["http://purl.obolibrary.org/obo/MI_2136"],
+            },
+            {
+                "id": "hERG_inhib",
+                "description": """whether it blocks (1) or not blocks (0) hERG
+(This is equivalent to whether hERG_at_10microM < -50, i.e.,
+whether the compound has an IC50 of less than 10microM.)""",
+                "units": None,
+                "type": "boolean",
+                "names": [
+                    "hERG inhibition",
+                    "Human ether-a-go-go related gene (hERG) inhibition",
+                    "hERG blocker",
+                    "Human ether-a-go-go related gene (hERG) blocker",
+                    "inhibition of Human ether-a-go-go related gene (hERG)",
+                ],
+                "uris": [
+                    "http://purl.obolibrary.org/obo/MI_2136",
+                ],
+            },
         ],
-                },
-                {
-                    "id": "hERG_at_10uM",  # name of the column in a tabular dataset
-                    "description": "The percent inhibition at a 10microM concentration",  # description of what this column means
-                    "units": "1microM concentration",  # units of the values in this column (leave empty if unitless)
-                    "type": "continuous",  # can be "categorical", "ordinal", "continuous"
-                    "names": [  # names for the property (to sample from for building the prompts)
-                        "hERG activity",
-                        "hERG active compound",
-                        "hERG blocker",
-                        "Human ether-a-go-go related gene (hERG) blocker",
-                        "Activity against Human ether-a-go-go related gene (hERG)",
-                        "hERG at 10microM",
-                        "hERG activity 10microM",
-                        "The percent inhibition at a 10microM concentration",
-                        "Compound percent activity at 10microM"
-                    ],
-                "uris":[
-                "https://bioportal.bioontology.org/ontologies/MI?p=classes&conceptid=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FMI_2136",
+        "identifiers": [
+            {
+                "id": "SMILES",  # column name
+                "type": "SMILES",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
+                "description": "SMILES",  # description (optional, except for "Other")
+            },
         ],
-                },
-                {
-                    "id": "hERG_inhib",  # name of the column in a tabular dataset
-                    "description": "whether it blocks (1) or not blocks (0). This is equivalent to whether hERG_at_10microM < -50, i.e. whether the compound has an IC50 of less than 10microM.",  # description of what this column means
-                    "units": "1microM concentration",  # units of the values in this column (leave empty if unitless)
-                    "type": "categorical",  # can be "categorical", "ordinal", "continuous"
-                    "names": [  # names for the property (to sample from for building the prompts)
-                        "hERG activity",
-                        "hERG active compound",
-                        "hERG blocker",
-                        "Human ether-a-go-go related gene (hERG) blocker",
-                        "Activity against Human ether-a-go-go related gene (hERG)",
-                    ],
-                "uris":[
-                "https://bioportal.bioontology.org/ontologies/MI?p=classes&conceptid=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FMI_2136",
+        "license": "CC BY 4.0",  # license under which the original dataset was published
+        "links": [  # list of relevant links (original dataset, other uses, etc.)
+            {
+                "url": "https://doi.org/10.1089/adt.2011.0425",
+                "description": "corresponding publication",
+            },
+            {
+                "url": "https://bbirnbaum.com/",
+                "description": "TDC Contributer",
+            },
+            {
+                "url": "https://tdcommons.ai/single_pred_tasks/tox/#herg-central",
+                "description": "Data source",
+            },
         ],
-                },
-            ],
-            "identifiers": [
-                {
-                    "id": "SMILES",  # column name
-                    "type": "SMILES",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
-                    "description": "SMILES",  # description (optional, except for "Other")
-                },
-            ],
-            "license": "CC BY 4.0",  # license under which the original dataset was published
-            "links": [  # list of relevant links (original dataset, other uses, etc.)
-                {
-                    "url": "https://doi.org/10.1089/adt.2011.0425",
-                    "description": "corresponding publication",
-                },
-                {
-                    "url": "https://bbirnbaum.com/",
-                    "description": "TDC Contributer",
-                },
-                {
-                    "url": "https://tdcommons.ai/single_pred_tasks/tox/#herg-central",
-                    "description": "Data source",
-                },
-            ],
-            "num_points": len(df),  # number of datapoints in this dataset
-            "bibtex": [
+        "num_points": len(df),  # number of datapoints in this dataset
+        "bibtex": [
             """@article{Du2011,
 doi = {10.1089/adt.2011.0425},
 url = {https://doi.org/10.1089/adt.2011.0425},
@@ -154,8 +143,8 @@ and Shunyou Long and Min Li},
 title = {hERGCentral: A Large Database to Store,  Retrieve,  and Analyze Compound Human
 Ether-a-go-go Related Gene Channel Interactions to Facilitate Cardiotoxicity Assessment in Drug Development},
 journal = {ASSAY and Drug Development Technologies}""",
-            ],
-        }
+        ],
+    }
 
     def str_presenter(dumper, data):
         """configures yaml for dumping multiline strings
