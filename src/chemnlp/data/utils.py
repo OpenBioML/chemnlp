926375e89e23132f54f2b7299dd6f823a5e76c0a
Jack Butler <jackapbutler@gmail.com>
2023-04-04 14:56:02 +0000

    add pad token id

--- src/chemnlp/data/utils.py
+++ src/chemnlp/data/utils.py
@@ -1,10 +1,11 @@
-import random
 import itertools
-
-from datasets import concatenate_datasets
-from datasets.formatting.formatting import LazyBatch
+import random
+from typing import Dict, List
 
 import chemnlp.data.hf_datasets as hf_datasets
+from datasets import concatenate_datasets
+from datasets.formatting.formatting import LazyBatch
+from transformers import PreTrainedTokenizer
 
 
 def sample_dataset(dataset, num_samples):
@@ -32,36 +33,35 @@ def chunks(lst, n):
         yield lst[i : i + n - 1]
 
 
-def pad_sequence(sequence, max_len):
+def pad_sequence(sequence, max_len, pad_token_id):
     """Pad a input sequence"""
     num_pad_tokens = max_len - len(sequence)
     attention_mask = [1] * len(sequence) + [0] * num_pad_tokens
-    sequence += [0] * num_pad_tokens
+    sequence += [pad_token_id] * num_pad_tokens
     return sequence, attention_mask
 
-def tokenise(batch: LazyBatch, tokenizer, max_length: int, string_key: str):
-        """Tokenise a batch of data using sample chunking"""
-        tok_articles = [tokenizer(x)["input_ids"] for x in batch[string_key]]
-        tok_articles = list(itertools.chain.from_iterable(tok_articles))
-        tok_articles = list(chunks(tok_articles, max_length))
 
-        padded_sequences_all = []
-        attention_masks_all = []
+def tokenise(batch: LazyBatch, tokenizer: PreTrainedTokenizer, max_length: int, string_key: str) -> Dict[str, List]:
+    """Tokenise a batch of data using sample chunking"""
+    tok_articles = [tokenizer(x)["input_ids"] for x in batch[string_key]]
+    tok_articles = list(itertools.chain.from_iterable(tok_articles))
+    tok_articles = list(chunks(tok_articles, max_length))
 
-        # Since articles are stitched together at the batch level
-        # we might need to pad the last article
-        for article in tok_articles:
-            if len(article) != max_length:
-                article, attention_masks = pad_sequence(
-                    article, max_length
-                )
-            else:
-                attention_masks = [1] * max_length
-            padded_sequences_all.append(article)
-            attention_masks_all.append(attention_masks)
+    padded_sequences_all = []
+    attention_masks_all = []
 
-        return {
-            "input_ids": padded_sequences_all,
-            "token_type_ids": [[0] * max_length] * len(padded_sequences_all),
-            "attention_mask": attention_masks_all,
-        }
+    # Since articles are stitched together at the batch level
+    # we might need to pad the last article
+    for article in tok_articles:
+        if len(article) != max_length:
+            article, attention_masks = pad_sequence(article, max_length, tokenizer.pad_token_id)
+        else:
+            attention_masks = [1] * max_length
+        padded_sequences_all.append(article)
+        attention_masks_all.append(attention_masks)
+
+    return {
+        "input_ids": padded_sequences_all,
+        "token_type_ids": [[0] * max_length] * len(padded_sequences_all),
+        "attention_mask": attention_masks_all,
+    }
