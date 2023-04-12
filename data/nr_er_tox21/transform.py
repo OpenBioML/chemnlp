faa1d47ea11608b69d19c8a6fc9d7837c5b71b62
Michael Pieler <Michael.Pieler@Gmail.com>
2023-04-12 19:04:51 +0200

    feat: nr_er_tox21 clean up

--- data/nr_er_tox21/transform.py
+++ data/nr_er_tox21/transform.py
@@ -2,7 +2,6 @@ import pandas as pd
 import yaml
 from tdc.single_pred import Tox
 from tdc.utils import retrieve_label_name_list
-import os
 
 
 def get_and_transform_data():
@@ -37,11 +36,6 @@ def get_and_transform_data():
     fields_clean = ["compound_id", "SMILES", f"toxicity_{target_subfolder}", "split"]
     df.columns = fields_clean
 
-    # data cleaning
-    #     df.compound_name = (
-    #         df.compound_name.str.strip()
-    #     )
-    # remove leading and trailing white space characters
     df = df.dropna()
     assert not df.duplicated().sum()
 
@@ -58,26 +52,19 @@ response pathways.""",
         "targets": [
             {
                 "id": f"toxicity_{target_subfolder}",  # name of the column in a tabular dataset
-                "description": "whether it toxic in a specific assay (1) or not toxic (0)",  # description of what this column means
+                "description": "whether it shows activity in the NR-ER toxicity assay (1) or not (0)",
                 "units": "toxicity",  # units of the values in this column (leave empty if unitless)
                 "type": "categorical",  # can be "categorical", "ordinal", "continuous"
                 "names": [  # names for the property (to sample from for building the prompts)
+                    "Tox21 R-ER toxicity",
                     "NR-ER toxicity",
-                    "NR-ER",
-                    "Tox21",
-                    "Tox21 NR-Estrogen receptor alpha",
-                    "NR-Estrogen receptor alpha",
-                    "Estrogen receptor alpha",
+                    "Tox21 NR-Estrogen receptor alpha toxicity",
+                    "NR-Estrogen receptor alpha toxicity",
                     "Estrogen receptor alpha toxicity",
-                    "Estrogen receptor alpha using the BG1 cell line",
                 ],
             },
         ],
-        "uris": [
-            "https://bioportal.bioontology.org/ontologies/NCIT?p=classes&conceptid=http%3A%2F%2Fncicb.nci.nih.gov%2Fxml%2Fowl%2FEVS%2FThesaurus.owl%23C38361",
-            "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-018-2523-5/tables/3",
-            "https://bioportal.bioontology.org/ontologies/CLO?p=classes&conceptid=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FCLO_0037345",
-        ],
+        "uris": None,
         "benchmarks": [
             {
                 "name": "TDC",  # unique benchmark name
@@ -104,7 +91,7 @@ response pathways.""",
             },
             {
                 "url": "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-018-2523-5/tables/3",
-                "description": "Assay name",
+                "description": "assay name",
             },
         ],
         "num_points": len(df),  # number of datapoints in this dataset
