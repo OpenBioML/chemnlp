6bac5fb89c261b995338480495115de469559990
Michael Pieler <Michael.Pieler@Gmail.com>
2023-04-12 19:15:52 +0200

    feat: sr_atad5_tox21 clean up

--- data/sr_atad5_tox21/transform.py
+++ data/sr_atad5_tox21/transform.py
@@ -2,7 +2,6 @@ import pandas as pd
 import yaml
 from tdc.single_pred import Tox
 from tdc.utils import retrieve_label_name_list
-import os
 
 
 def get_and_transform_data():
@@ -37,11 +36,6 @@ def get_and_transform_data():
     fields_clean = ["compound_id", "SMILES", f"toxicity_{target_subfolder}", "split"]
     df.columns = fields_clean
 
-    # data cleaning
-    #     df.compound_name = (
-    #         df.compound_name.str.strip()
-    #     )
-    # remove leading and trailing white space characters
     df = df.dropna()
     assert not df.duplicated().sum()
 
@@ -58,25 +52,19 @@ response pathways.""",
         "targets": [
             {
                 "id": f"toxicity_{target_subfolder}",  # name of the column in a tabular dataset
-                "description": "whether it toxic in a specific assay (1) or not toxic (0)",  # description of what this column means
+                "description": f"whether it shows activitiy in the {target_subfolder} assay (1) or not (0)",
                 "units": "toxicity",  # units of the values in this column (leave empty if unitless)
                 "type": "categorical",  # can be "categorical", "ordinal", "continuous"
                 "names": [  # names for the property (to sample from for building the prompts)
                     f"{target_subfolder} toxicity",
-                    f"{target_subfolder}",
-                    "Tox21",
-                    "Tox21 SR-Luciferase-tagged ATAD5",
-                    "SR-Luciferase-tagged ATAD5 in human embryonic kidney cells",
-                    "Luciferase-tagged ATAD5 in human embryonic kidney cells assay",
+                    "Tox21 SR-Luciferase-tagged ATAD5 toxicity",
+                    "SR-Luciferase-tagged ATAD5 in human embryonic kidney cells toxicity",
+                    "Luciferase-tagged ATAD5 in human embryonic kidney cells toxicity",
                     "Luciferase-tagged ATAD5 toxicity",
                 ],
             },
         ],
-        "uris": [
-            "https://bioportal.bioontology.org/ontologies/PR?p=classes&conceptid=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FHGNC_25752",
-            "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-018-2523-5/tables/3",
-            "https://bioportal.bioontology.org/ontologies/MESH?p=classes&conceptid=http%3A%2F%2Fpurl.bioontology.org%2Fontology%2FMESH%2FD000066449",
-        ],
+        "uris": None,
         "benchmarks": [
             {
                 "name": "TDC",  # unique benchmark name
@@ -103,7 +91,7 @@ response pathways.""",
             },
             {
                 "url": "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-018-2523-5/tables/3",
-                "description": "Assay name",
+                "description": "assay name",
             },
         ],
         "num_points": len(df),  # number of datapoints in this dataset
