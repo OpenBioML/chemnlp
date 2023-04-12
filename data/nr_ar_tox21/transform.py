7b267babaaf3d1e7279f6314b9d95fc943ee9d33
Michael Pieler <Michael.Pieler@Gmail.com>
2023-04-12 16:02:32 +0200

    feat: nr_ar_tox21 clean up

--- data/nr_ar_tox21/transform.py
+++ data/nr_ar_tox21/transform.py
@@ -2,7 +2,6 @@ import pandas as pd
 import yaml
 from tdc.single_pred import Tox
 from tdc.utils import retrieve_label_name_list
-import os
 
 
 def get_and_transform_data():
@@ -37,12 +36,6 @@ def get_and_transform_data():
     fields_clean = ["compound_id", "SMILES", f"toxicity_{target_subfolder}", "split"]
     df.columns = fields_clean
 
-    # data cleaning
-    #     df.compound_name = (
-    #         df.compound_name.str.strip()
-    #     )
-    # remove leading and trailing white space characters
-
     assert not df.duplicated().sum()
 
     # save to csv
@@ -58,23 +51,17 @@ response pathways.""",
         "targets": [
             {
                 "id": f"toxicity_{target_subfolder}",  # name of the column in a tabular dataset
-                "description": "whether it toxic in a specific assay (1) or not toxic (0)",  # description of what this column means
+                "description": "whether it toxic in a androgen receptor toxicity assay (1) or not (0)",
                 "units": "toxicity",  # units of the values in this column (leave empty if unitless)
                 "type": "categorical",  # can be "categorical", "ordinal", "continuous"
                 "names": [  # names for the property (to sample from for building the prompts)
+                    "Tox21 NR-AR toxicity",
                     "NR-AR toxicity",
-                    "NR-AR",
-                    "Tox21",
-                    "Tox21 NR-AR",
-                    "NR-androgen receptor",
-                    "androgen receptor assay",
-                    "androgen receptor toxicity",
+                    "NR-androgen receptor toxicity",
                 ],
             },
         ],
-        "uris": [
-            "https://bioportal.bioontology.org/ontologies/ORDO/?p=classes&conceptid=http%3A%2F%2Fwww.orpha.net%2FORDO%2FOrphanet_132285",
-        ],
+        "uris": None,
         "benchmarks": [
             {
                 "name": "TDC",  # unique benchmark name
@@ -101,7 +88,7 @@ response pathways.""",
             },
             {
                 "url": "https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-018-2523-5/tables/3",
-                "description": "Assay name",
+                "description": "assay name",
             },
         ],
         "num_points": len(df),  # number of datapoints in this dataset
