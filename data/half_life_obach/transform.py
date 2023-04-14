40769841a02ee3d991739e02b190c74520529685
Michael Pieler <Michael.Pieler@Gmail.com>
2023-04-14 11:25:46 +0200

    feat: half_life_obach clean up

--- data/half_life_obach/transform.py
+++ data/half_life_obach/transform.py
@@ -2,10 +2,11 @@ import pandas as pd
 import yaml
 from tdc.single_pred import ADME
 
+
 def get_and_transform_data():
     # get raw data
-    target_subfolder = 'Half_Life_Obach'
-    splits = ADME(name = target_subfolder).get_split()
+    target_subfolder = "Half_Life_Obach"
+    splits = ADME(name=target_subfolder).get_split()
     df_train = splits["train"]
     df_valid = splits["valid"]
     df_test = splits["test"]
@@ -23,30 +24,21 @@ def get_and_transform_data():
         fn_data_original,
         delimiter=",",
     )  # not necessary but ensure we can load the saved data
-    
+
     # check if fields are the same
     fields_orig = df.columns.tolist()
-    assert fields_orig == ['Drug_ID', 
-                           'Drug', 
-                           'Y',
-                           'split'
-                           ]
+    assert fields_orig == ["Drug_ID", "Drug", "Y", "split"]
 
     # overwrite column names = fields
-    fields_clean = ['chembl_id', 
-                    'SMILES', 
-                    'half_life_duration',
-                    'split']
+    fields_clean = ["chembl_id", "SMILES", "half_life_duration", "split"]
     df.columns = fields_clean
 
     # data cleaning
-    df[fields_clean[0]] = (
-        df[fields_clean[0]].str.strip()
-    )  
     # remove leading and trailing white space characters
+    df.chembl_id = df.chembl_id.str.strip()
     df = df.dropna()
     assert not df.duplicated().sum()
-    
+
     # save to csv
     fn_data_csv = "data_clean.csv"
     df.to_csv(fn_data_csv, index=False)
@@ -57,30 +49,25 @@ in the body to be reduced by half. It measures the duration of actions of a drug
 This dataset deposited version under CHEMBL assay 1614674.""",
         "targets": [
             {
-    "id": "half_life_duration",  # name of the column in a tabular dataset
-    "description": "The half-life (t1/2) is the time it takes for the plasma concentration of a drug in the body to be reduced by 50%",  # description of what this column means
-    "units": "(t1/2) in hours",  # units of the values in this column (leave empty if unitless)
-    "type": "continuous",  # can be "categorical", "ordinal", "continuous"
-    "names": [  # names for the property (to sample from for building the prompts)
-        "drug half time",
-        "drug t half",
-        "The duration by which the concentration of the drug in the body reduced by half",
-        "ADME half life",
-        "drug (t1/2)",
-        "Pharmacokinetics drug half life",
-        "Drug Excretion",
-    ],
-    "uris":[
-            "https://bioportal.bioontology.org/ontologies/MESH?p=classes&conceptid=http%3A%2F%2Fpurl.bioontology.org%2Fontology%2FMESH%2FD006207",
-            "https://bioportal.bioontology.org/ontologies/NCIT?p=classes&conceptid=http%3A%2F%2Fncicb.nci.nih.gov%2Fxml%2Fowl%2FEVS%2FThesaurus.owl%23C94618",
+                "id": "half_life_duration",  # name of the column in a tabular dataset
+                "description": "the time it takes for the plasma concentration of a drug in the body to be reduced by half",  # noqa: E501
+                "units": "hours",  # units of the values in this column (leave empty if unitless)
+                "type": "continuous",  # can be "categorical", "ordinal", "continuous"
+                "names": [  # names for the property (to sample from for building the prompts)
+                    "drug half life time",
+                    "the duration by which the concentration of the drug in the body is reduced by half",
+                    "half life",
+                ],
+                "uris": [
+                    "http://purl.bioontology.org/ontology/MESH/D006207",
                 ],
             },
         ],
         "benchmarks": [
             {
-        "name": "TDC",  # unique benchmark name
-        "link": "https://tdcommons.ai/",  # benchmark URL
-        "split_column": "split",  # name of the column that contains the split information
+                "name": "TDC",  # unique benchmark name
+                "link": "https://tdcommons.ai/",  # benchmark URL
+                "split_column": "split",  # name of the column that contains the split information
             },
         ],
         "identifiers": [
@@ -92,13 +79,9 @@ This dataset deposited version under CHEMBL assay 1614674.""",
             {
                 "id": "chembl_id",  # column name
                 "type": "Other",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
-                "names": [
-                "ChEMBL database id",
-                "ChEMBL identifier number"
-                ], 
+                "names": ["ChEMBL database id", "ChEMBL identifier number"],
                 "description": "ChEMBL ids",  # description (optional, except for "Other")
             },
-
         ],
         "license": "CC BY 4.0",  # license under which the original dataset was published
         "links": [  # list of relevant links (original dataset, other uses, etc.)
@@ -109,7 +92,7 @@ This dataset deposited version under CHEMBL assay 1614674.""",
             {
                 "url": "https://tdcommons.ai/single_pred_tasks/adme/#half-life-obach-et-al",
                 "description": "data source",
-            }
+            },
         ],
         "num_points": len(df),  # number of datapoints in this dataset
         "bibtex": [
@@ -126,9 +109,9 @@ author = {R. Scott Obach and Franco Lombardo and Nigel J. Waters},
 title = {Trend Analysis of a Database of Intravenous Pharmacokinetic
 Parameters in Humans for 670 Drug Compounds},
 journal = {Drug Metabolism and Disposition}""",
-          ],
+        ],
     }
-    
+
     def str_presenter(dumper, data):
         """configures yaml for dumping multiline strings
         Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
@@ -146,6 +129,7 @@ journal = {Drug Metabolism and Disposition}""",
         yaml.dump(meta, f, sort_keys=False)
 
     print(f"Finished processing {meta['name']} dataset!")
+
 
 if __name__ == "__main__":
     get_and_transform_data()
