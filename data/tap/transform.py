f141055df56d174195f37a137166906bdc97fa45
Michael Pieler <Michael.Pieler@Gmail.com>
2023-04-19 13:44:25 +0200

    feat: tap clean up train split setup

--- data/tap/transform.py
+++ data/tap/transform.py
@@ -8,15 +8,47 @@ def get_and_transform_data():
     # get raw data
     target_subfolder = "TAP"
     label_list = retrieve_label_name_list(target_subfolder)
-    data = Develop(name=target_subfolder, label_name=label_list[0])
+    df = pd.DataFrame()
+    for i, label in enumerate(label_list):
+        print(f"Get data subset {label}:")
+        splits = Develop(name=target_subfolder, label_name=label).get_split()
+        df_train = splits["train"]
+        df_valid = splits["valid"]
+        df_test = splits["test"]
+        df_train["split"] = "train"
+        df_valid["split"] = "valid"
+        df_test["split"] = "test"
+        df_cat = pd.concat([df_train, df_valid, df_test], axis=0)
+        assert df_cat.columns.tolist() == ["Antibody_ID", "Antibody", "Y", "split"]
+        df_cat.columns = ["Antibody_ID", "Antibody", label, "split"]
+        if i > 0:
+            df = pd.merge(df, df_cat, on=["Antibody_ID", "Antibody", "split"])
+        else:
+            df = df_cat
+
+    fn_data_raw = "data_raw.csv"
+    df.to_csv(fn_data_raw, index=False)
+    del df
+
     # proceed raw data
-    df = pd.read_csv("data/tap.tab", sep="\t")
+    df = pd.read_csv(fn_data_raw, sep=",")
     fields_orig = df.columns.tolist()
-    assert fields_orig == ["X", "ID", "CDR_Length", "PSH", "PPC", "PNC", "SFvCSP"]
-    fields_clean = [
-        "antibody_two_sequences",
-        "antibody_name",
+
+    assert fields_orig == [
+        "Antibody_ID",
+        "Antibody",
         "CDR_Length",
+        "split",
+        "PSH",
+        "PPC",
+        "PNC",
+        "SFvCSP",
+    ]
+    fields_clean = [
+        "antibody_name",
+        "antibody_sequences",
+        "CDR_Length",
+        "split",
         "PSH",
         "PPC",
         "PNC",
@@ -24,13 +56,14 @@ def get_and_transform_data():
     ]
     df.columns = fields_clean
     #  convert list columns to two columns
-    antibody_list = df.antibody_two_sequences.tolist()
-    s2l = lambda list_string: list(
-        map(str.strip, list_string.strip("][").replace("'", "").split(","))
-    )
-    antibody2list = lambda list_string: [
-        x.strip() for x in s2l(list_string)[0].split("\\n")
-    ]
+    antibody_list = df.antibody_sequences.tolist()
+
+    def s2l(list_string):
+        return list(map(str.strip, list_string.strip("][").replace("'", "").split(",")))
+
+    def antibody2list(list_string):
+        return [x.strip() for x in s2l(list_string)[0].split("\\n")]
+
     df["heavy_chain"] = [antibody2list(x)[0] for x in antibody_list]
     df["light_chain"] = [antibody2list(x)[1] for x in antibody_list]
     fn_data_original = "data_original.csv"
@@ -40,9 +73,10 @@ def get_and_transform_data():
     df = pd.read_csv(fn_data_original, sep=",")
     fields_orig = df.columns.tolist()
     assert fields_orig == [
-        "antibody_two_sequences",
         "antibody_name",
+        "antibody_sequences",
         "CDR_Length",
+        "split",
         "PSH",
         "PPC",
         "PNC",
@@ -57,6 +91,7 @@ def get_and_transform_data():
             "heavy_chain",
             "light_chain",
             "CDR_Length",
+            "split",
             "PSH",
             "PPC",
             "PNC",
@@ -68,6 +103,7 @@ def get_and_transform_data():
         "heavy_chain",
         "light_chain",
         "CDR_Length",
+        "split",
         "PSH",
         "PPC",
         "PNC",
@@ -95,113 +131,110 @@ clinical-stage mAb therapeutics.""",
         "targets": [
             {
                 "id": "CDR_Length",  # name of the column in a tabular dataset
-                "description": "CDR Complementarity-determining regions length", 
-                "units": "",  # units of the values in this column (leave empty if unitless)
+                "description": "complementarity-determining regions (CDR) length",
+                "units": "amino acids",  # units of the values in this column (leave empty if unitless)
                 "type": "continuous",  # can be "categorical", "ordinal", "continuous"
                 "names": [  # names for the property (to sample from for building the prompts)
-                    "Antibody Complementarity-determining regions length",
-                    "Therapeutic Antibody Profiler",
-                    "antibody developability",
-                    "monoclonal anitbody",
+                    "antibody complementarity-determining regions length",
+                    "antibody complementarity-determining regions (CDR) length",
+                    "antibody CDR length",
+                    "complementarity-determining regions (CDR) length",
+                    "complementarity-determining regions length",
+                    "CDR length",
                 ],
-                "uris": [
-                    "https://rb.gy/s9gv88",
-                    "https://rb.gy/km77hq",
-                    "https://rb.gy/b8cx8i",
-                ],
+                "uris": None,
             },
             {
                 "id": "PSH",  # name of the column in a tabular dataset
-                "description": "patches of surface hydrophobicity",  
-                "units": "",  # units of the values in this column (leave empty if unitless)
+                "description": "patches of surface hydrophobicity (PSH) score",
+                "units": None,  # units of the values in this column (leave empty if unitless)
                 "type": "continuous",  # can be "categorical", "ordinal", "continuous"
                 "names": [  # names for the property (to sample from for building the prompts)
-                    "antibody patches of surface hydrophobicity",
-                    "Therapeutic Antibody Profiler",
-                    "antibody developability",
-                    "monoclonal anitbody",
+                    "antibody patches of surface hydrophobicity (PSH) score",
+                    "antibody patches of surface hydrophobicity score",
+                    "antibody PSH score",
+                    "patches of surface hydrophobicity (PSH) score",
+                    "patches of surface hydrophobicity score",
+                    "PSH score",
                 ],
-                "uris": [
-                    "https://rb.gy/bchhaa",
-                    "https://rb.gy/2irr4l",
-                    "https://rb.gy/b8cx8i",
-                ],
+                "uris": None,
             },
             {
                 "id": "PPC",  # name of the column in a tabular dataset
-                "description": "patches of positive charge", 
-                "units": "",  # units of the values in this column (leave empty if unitless)
+                "description": "patches of positive charge (PPC) score",
+                "units": None,  # units of the values in this column (leave empty if unitless)
                 "type": "continuous",  # can be "categorical", "ordinal", "continuous"
                 "names": [  # names for the property (to sample from for building the prompts)
-                    "patches of positive charge",
-                    "Therapeutic Antibody Profiler",
-                    "antibody developability",
-                    "monoclonal anitbody",
+                    "antibody patches of positive charge (PPC) score",
+                    "antibody patches of positive charge score",
+                    "antibody PPC score",
+                    "patches of positive charge (PPC) score",
+                    "patches of positive charge score",
+                    "PPC score",
                 ],
-                "uris": [
-                    "https://rb.gy/b8cx8i",
-                ],
+                "uris": None,
             },
             {
                 "id": "PNC",  # name of the column in a tabular dataset
-                "description": "patches of negative charge", 
-                "units": "",  # units of the values in this column (leave empty if unitless)
+                "description": "patches of negative charge (PNC) score",
+                "units": None,  # units of the values in this column (leave empty if unitless)
                 "type": "continuous",  # can be "categorical", "ordinal", "continuous"
                 "names": [  # names for the property (to sample from for building the prompts)
-                    "anitbody patches of negative charge",
-                    "Therapeutic Antibody Profiler",
-                    "antibody developability",
-                    "monoclonal anitbody",
+                    "antibody patches of negative charge (PNC) score",
+                    "antibody patches of negative charge score",
+                    "antibody PNC score",
+                    "patches of negative charge (PNC) score",
+                    "patches of negative charge score",
+                    "PNC score",
                 ],
-                "uris": [
-                    "https://rb.gy/b8cx8i",
-                ],
+                "uris": None,
             },
             {
                 "id": "SFvCSP",  # name of the column in a tabular dataset
-                "description": "structural Fv charge symmetry parameter",
-                "units": "",  # units of the values in this column (leave empty if unitless)
+                "description": "structural Fv charge symmetry parameter (SFvCSP) score",
+                "units": None,  # units of the values in this column (leave empty if unitless)
                 "type": "continuous",  # can be "categorical", "ordinal", "continuous"
                 "names": [  # names for the property (to sample from for building the prompts)
-                    "antibody structural Fv charge symmetry parameter",
-                    "Therapeutic Antibody Profiler",
-                    "antibody developability",
-                    "monoclonal anitbody",
+                    "antibody structural Fv charge symmetry parameter (SFvCSP) score",
+                    "antibody structural Fv charge symmetry parameter score",
+                    "antibody SFvCSP score",
+                    "structural Fv charge symmetry parameter (SFvCSP) score",
+                    "structural Fv charge symmetry parameter score",
+                    "SFvCSP score",
                 ],
-                "uris": [
-                    "https://rb.gy/uxyhc3",
-                    "https://rb.gy/b8cx8i",
-                ],
+                "uris": None,
             },
         ],
         "identifiers": [
             {
                 "id": "antibody_name",  # column name
                 "type": "Other",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
-                "names":[
-                "Name of the antibody",
-                         "Name of the antibody drug",
-                         "Name of drug"
+                "names": [
+                    "antibody name",
+                    "name of the antibody",
+                    "name of the antibody drug",
                 ],
-                "description": "anitbody name",  # description (optional, except for "Other")
+                "description": "antibody name",  # description (optional, except for "Other")
             },
             {
                 "id": "heavy_chain",  # column name
                 "type": "Other",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
-                "names":[
-                "Fastq",
-                "gene sequence",
+                "names": [
+                    "amino acid sequence",
+                    "heavy chain amino acid sequence",
+                    "heavy chain AA sequence",
                 ],
-                "description": "anitbody heavy chain amino acid sequence",  # description (optional, except for "Other")
+                "description": "antibody heavy chain amino acid sequence",  # description (optional, except for "Other")
             },
             {
                 "id": "light_chain",  # column name
                 "type": "Other",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
-                "names":[
-                "Fastq",
-                "gene sequence",
+                "names": [
+                    "amino acid sequence",
+                    "light chain amino acid sequence",
+                    "light chain AA sequence",
                 ],
-                "description": "anitbody light chain amino acid sequence",  # description (optional, except for "Other")
+                "description": "antibody light chain amino acid sequence",  # description (optional, except for "Other")
             },
         ],
         "license": "CC BY 4.0",  # license under which the original dataset was published
