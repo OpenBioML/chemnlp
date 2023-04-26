ab0352811ef176d6b3478824a0d52ee607d51c6e
Mohamed Abd Elaleem <109590482+phalem@users.noreply.github.com>
2023-04-26 18:39:58 +0200

    Add HTS HIV data (#60)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Michael Pieler <Michael.Pieler@Gmail.com>

---
+++ data/hiv/transform.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import yaml
+from tdc.single_pred import HTS
+
+
+def get_and_transform_data():
+    # get raw data
+    splits = HTS(name="HIV").get_split()
+    df_train = splits["train"]
+    df_valid = splits["valid"]
+    df_test = splits["test"]
+    df_train["split"] = "train"
+    df_valid["split"] = "valid"
+    df_test["split"] = "test"
+    df = pd.concat([df_train, df_valid, df_test], axis=0)
+
+    fn_data_original = "data_original.csv"
+    df.to_csv(fn_data_original, index=False)
+    del df
+
+    # create dataframe
+    df = pd.read_csv(
+        fn_data_original,
+        delimiter=",",
+    )  # not necessary but ensure we can load the saved data
+
+    # check if fields are the same
+    fields_orig = df.columns.tolist()
+    assert fields_orig == [
+        "Drug_ID",
+        "Drug",
+        "Y",
+        "split",
+    ]
+
+    # overwrite column names = fields
+    fields_clean = [
+        "compound_id",
+        "SMILES",
+        "activity_HIV",
+        "split",
+    ]
+    df.columns = fields_clean
+
+    # data cleaning
+    df.compound_id = (
+        df.compound_id.str.strip()
+    )  # remove leading and trailing white space characters
+
+    assert not df.duplicated().sum()
+
+    # save to csv
+    fn_data_csv = "data_clean.csv"
+    df.to_csv(fn_data_csv, index=False)
+
+    # create meta yaml
+    meta = {
+        "name": "hiv",  # unique identifier, we will also use this for directory names
+        "description": """The HIV dataset was introduced by the Drug Therapeutics Program (DTP)
+AIDS Antiviral Screen, which tested the ability to inhibit HIV replication for
+over 40,000 compounds.""",
+        "targets": [
+            {
+                "id": "activity_HIV",  # name of the column in a tabular dataset
+                "description": "whether it is active against HIV virus (1) or not (0).",
+                "units": None,  # units of the values in this column (leave empty if unitless)
+                "type": "boolean",  # can be "categorical", "ordinal", "continuous"
+                "names": [  # names for the property (to sample from for building the prompts)
+                    {"noun": "activity against the human immunodeficiency virus"},
+                    {"noun": "activity against HIV"},
+                    {"adjective": "active against the human immunodeficiency virus"},
+                    {"adjective": "active against HIV"},
+                ],
+            },
+        ],
+        "benchmarks": [
+            {
+                "name": "TDC",  # unique benchmark name
+                "link": "https://tdcommons.ai/",  # benchmark URL
+                "split_column": "split",  # name of the column that contains the split information
+            },
+        ],
+        "identifiers": [
+            {
+                "id": "SMILES",  # column name
+                "type": "SMILES",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
+                "description": "SMILES",  # description (optional, except for "Other")
+            },
+        ],
+        "license": "CC BY 4.0",  # license under which the original dataset was published
+        "links": [  # list of relevant links (original dataset, other uses, etc.)
+            {
+                "url": "https://rb.gy/wphpqg",
+                "description": "data source",
+            },
+            {
+                "url": "https://rb.gy/0xx91v",
+                "description": "corresponding publication",
+            },
+            {
+                "url": "https://tdcommons.ai/single_pred_tasks/hts/#hiv",
+                "description": "data source",
+            },
+        ],
+        "num_points": len(df),  # number of datapoints in this dataset
+        "bibtex": [
+            """@article{Wu2018,
+doi = {10.1039/c7sc02664a},
+url = {https://doi.org/10.1039/c7sc02664a},
+year = {2018},
+publisher = {Royal Society of Chemistry (RSC)},
+volume = {9},
+number = {2},
+pages = {513--530},
+author = {Zhenqin Wu and Bharath Ramsundar and Evan~N. Feinberg and Joseph Gomes
+and Caleb Geniesse and Aneesh S. Pappu and Karl Leswing and Vijay Pande},
+title = {MoleculeNet: a benchmark for molecular machine learning},
+journal = {Chemical Science}""",
+        ],
+    }
+
+    def str_presenter(dumper, data):
+        """configures yaml for dumping multiline strings
+        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+        """
+        if data.count("\n") > 0:  # check for multiline string
+            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+
+    yaml.add_representer(str, str_presenter)
+    yaml.representer.SafeRepresenter.add_representer(
+        str, str_presenter
+    )  # to use with safe_dum
+    fn_meta = "meta.yaml"
+    with open(fn_meta, "w") as f:
+        yaml.dump(meta, f, sort_keys=False)
+
+    print(f"Finished processing {meta['name']} dataset!")
+
+
+if __name__ == "__main__":
+    get_and_transform_data()
