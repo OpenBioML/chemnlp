92e906ad0ffd8845ea31acb4200210a044438b05
jack.butler <jack.butler@faculty.ai>
2023-08-15 10:12:22 +0100

    add sliding window sampler - tested locally on CPU

---
+++ src/chemnlp/sampler.py
@@ -0,0 +1,88 @@
+from typing import Iterator, Optional
+import torch.distributed as dist
+from torch.utils.data import sampler, Dataset
+import math
+
+
+class ChronologicalRepeatSampler(sampler.Sampler):
+    """
+    Sampler that restricts data loading to a subset of the dataset
+        * given the incoming sorted, tokenised data
+        * assume we want to sample chronologically
+        * and repeat the data indices N times
+
+                                dataset
+    | 0  1  2  3 | 4  5  6  7 | 8  9  10  11 | 12  13  14  15 |
+    |            |            |              |                |
+         gpu 1        gpu 2         gpu 3          gpu 4 
+    
+        * move the sliding window at varying pace
+
+    | 1  2  3  4 | 5  6  7  8 | 9  10  11  12 | 13  14  15  16 |
+    |            |            |               |                |
+         gpu 1        gpu 2         gpu 3          gpu 4 
+    """
+
+    def __init__(self, dataset: Dataset, num_repeats: int, num_replicas: Optional[int] = None,
+                 rank: Optional[int] = None,  drop_last: bool = False) -> None:
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            rank = dist.get_rank()
+        if rank >= num_replicas or rank < 0:
+            raise ValueError(
+                f"Invalid rank {rank}, rank should be in the interval [0, {num_replicas - 1}]")
+        self.dataset = dataset
+        self.num_repeats = num_repeats
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+        self.drop_last = drop_last
+        if self.drop_last and len(self.dataset) % self.num_replicas != 0:  # type: ignore[arg-type]
+            self.num_samples = math.ceil(
+                (len(self.dataset) - self.num_replicas) / self.num_replicas  # type: ignore[arg-type]
+            )
+        else:
+            self.num_samples = math.ceil(len(self.dataset) / self.num_replicas)  # type: ignore[arg-type]
+        self.total_size = self.num_samples * self.num_replicas * self.num_repeats
+
+    def _generate_windowed_indices(self):
+        # sliding window concatenation of size self.num_repeats
+        indices = list(range(len(self.dataset)))
+        sliding_windows = [indices[i:i+self.num_repeats] for i in range(0, len(indices))]
+        return [e for nestlist in sliding_windows for e in nestlist]
+
+    def _handle_uneven_lengths(self, indices):
+        # add extra samples to make it evenly divisible
+        padding_size = self.total_size - len(indices)
+        if padding_size <= len(indices):
+            indices += indices[:padding_size]
+        else:
+            indices += (indices * math.ceil(padding_size / len(indices)))[:padding_size]        
+        return indices
+
+    def __iter__(self) -> Iterator:
+        indices = self._generate_windowed_indices()
+        
+        if not self.drop_last:
+             indices = self._handle_uneven_lengths(indices)
+        else:
+            indices = indices[:self.total_size]
+        assert len(indices) == self.total_size
+
+        # subsample
+        sub_indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(sub_indices) == self.num_samples * self.num_repeats
+
+        return iter(sub_indices)
+
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def set_epoch(self, epoch: int) -> None:
+        self.epoch = epoch
+    
