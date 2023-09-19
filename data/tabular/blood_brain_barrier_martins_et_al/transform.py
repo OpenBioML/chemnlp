d3bab6b2df271291cc818614cb14786e688a4f03
Michael Pieler <Michael.Pieler@Gmail.com>
2023-09-19 18:40:43 +0200

    feat: add IT tasks for benchmarking

--- data/tabular/blood_brain_barrier_martins_et_al/transform.py
+++ data/tabular/blood_brain_barrier_martins_et_al/transform.py
@@ -185,6 +185,9 @@ Description: A molecule that is {penetrate_BBB__names__adjective}.
 {#Molecule |!}{SMILES__description}: {SMILES#}
 Constraint: Even if you are {#uncertain|not sure!}, you must pick either "True" or "False" without using any {#other|additional!} words.
 Result:<EOI> {penetrate_BBB#False&True}""",  # noqa: E501
+            """Task: Please {#give me|create|generate!} a molecule {SMILES__description} based on the {#text |!}description{# below|!}.
+Description: A molecule that is {penetrate_BBB__names__adjective}.
+Result:<EOI> {SMILES#}""",  # noqa: E501
             """Task: Please answer the multiple choice question.
 Question: Is the molecule with the {SMILES__description} of {SMILES#} {penetrate_BBB__names__adjective}?
 Constraint: Even if you are {#uncertain|not sure!}, you must pick either {%multiple_choice_enum%2%aA1} without using any {#other|additional!} words.
