f5b98c2321d78a26ed8f2f0c1e2bdfc14c458644
Adrian Mirza <adrianmirza81@gmail.com>
2023-09-25 11:15:34 +0200

    Update data/tabular/sarscov2_3clpro_diamond/transform.py
    Co-authored-by: Michael Pieler <36303596+MicPie@users.noreply.github.com>

--- data/tabular/sarscov2_3clpro_diamond/transform.py
+++ data/tabular/sarscov2_3clpro_diamond/transform.py
@@ -52,7 +52,7 @@ main protease at high resolution. From MIT AiCures.""",
                 "units": None,  # units of the values in this column (leave empty if unitless)
                 "type": "boolean",
                 "names": [  # names for the property (to sample from for building the prompts)
-                    {"noun": "activity against SARSCoV2 3CL protease"},
+                    {"noun": "activity against the SARSCoV2 3CL protease"},
                     {"noun": "activity against SARS-CoV-2 3CL protease"},
                     {"adjective": "active against SARSCoV2 3CL protease"},
                     {"adjective": "active against SARS-CoV-2 3CL protease"},
