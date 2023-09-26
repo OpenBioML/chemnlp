a7c959171071d8ec470e7dede6139ceecd3dbce2
Michael Pieler <Michael.Pieler@Gmail.com>
2023-09-26 17:53:28 +0200

    fix: yes no recoding cyp2c9_substrate_carbonmangels

--- data/tabular/cyp2c9_substrate_carbonmangels/transform.py
+++ data/tabular/cyp2c9_substrate_carbonmangels/transform.py
@@ -177,7 +177,7 @@ Assistant: {#This sounds very exciting. |This sounds very interesting. !}Should 
 User: Yes, the molecule should {CYP2C9_Substrate#not &NULL}be a {CYP2C9_Substrate__names__noun}.
 Assistant: {#Understood|Got it|Ok!}, this {SMILES__description} is {CYP2C9_Substrate#not &NULL}a {CYP2C9_Substrate__names__noun}: {SMILES#}""",  # noqa: E501
             # Benchmarking text templates
-            "Is the {SMILES__description} {SMILES#} a {CYP2C9_Substrate__names__noun}:<EOI> {CYP2C9_Substrate#yes&no}",  # noqa: E501 for the benchmarking setup <EOI> separates input and output
+            "Is the {SMILES__description} {SMILES#} a {CYP2C9_Substrate__names__noun}:<EOI> {CYP2C9_Substrate#no&yes}",  # noqa: E501 for the benchmarking setup <EOI> separates input and output
             """Task: Please classify a molecule based on the description.
 Description: A molecule that is a {CYP2C9_Substrate__names__noun}.
 {#Molecule |!}{SMILES__description}: {SMILES#}
