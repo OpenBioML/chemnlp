de7bf6926e039790f483e48ac624512195ab3ceb
Michael Pieler <Michael.Pieler@Gmail.com>
2023-09-28 09:53:27 +0200

    feat: update templates m1_muscarinic_receptor_agonists_butkiewicz

--- data/text_sampling/text_sampling.py
+++ data/text_sampling/text_sampling.py
@@ -95,6 +95,7 @@ exclude_from_standard_tabular_text_templates = [
     "human_intestinal_absorption",  # boolean target data
     "iupac_goldbook",  # text only, no SMILES
     "kcnq2_potassium_channel_butkiewicz",  # boolean target data
+    "m1_muscarinic_receptor_agonists_butkiewicz",  # boolean target data
     "moses",  # SMILES only, has no target
     "zinc",  # SMILES only, has no target
 ]
