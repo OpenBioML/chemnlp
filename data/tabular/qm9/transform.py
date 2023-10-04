35aff677287e491904c7008f71e018896a4b0181
n0w0f <pvt.nawaf@gmail.com>
2023-10-05 02:51:43 +0530

    feat: add transform.py to clean and prep data

---
+++ data/tabular/qm9/transform.py
@@ -0,0 +1,52 @@
+import datasets
+import pandas as pd
+
+DATASET_NAME = "qm9"
+
+
+def prepare_data():
+    dataset_name = "n0w0f/qm9-csv"
+    split_name = "train"  # data without any split @ hf
+    filename_to_save = "data_clean.csv"
+
+    # Load the dataset from Hugging Face
+    dataset = datasets.load_dataset(dataset_name, split=split_name)
+
+    df = pd.DataFrame(dataset)
+
+    # assert column names
+    fields_orig = df.columns.tolist()
+    assert fields_orig == [
+        "inchi",
+        "smiles",
+        "rotational_constant_a",
+        "rotational_constant_b",
+        "rotational_constant_c",
+        "dipole_moment",
+        "polarizability",
+        "homo",
+        "lumo",
+        "gap",
+        "r2",
+        "zero_point_energy",
+        "u0",
+        "u298",
+        "h298",
+        "g298",
+        "heat_capacity",
+    ]
+    assert not df.duplicated().sum()
+
+    # remove duplicates if any
+    df = df.drop_duplicates()
+
+    datapoints = len(df)
+
+    df.to_csv(filename_to_save, index=False)
+    return datapoints
+
+
+if __name__ == "__main__":
+    print(f" Preparing  clean tabular {DATASET_NAME} datatset")
+    prepare_data()
+    print(f" Finished Preparing  clean tabular {DATASET_NAME} datatset")
