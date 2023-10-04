ccb2fd6652c78343e53235c1caf363e9f94a6af7
Adrian Mirza <adrianmirza81@gmail.com>
2023-10-04 13:14:02 +0200

    Templates bicerano (#394)
    Co-authored-by: Kevin M Jablonka <32935233+kjappelbaum@users.noreply.github.com>
    Co-authored-by: Kevin Maik Jablonka <kevin.jablonka@epfl.ch>

--- data/tabular/bicerano_dataset/transform.py
+++ data/tabular/bicerano_dataset/transform.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from canonicalize_psmiles.canonicalize import canonicalize
 
 
 def transform_data():
-    original_data = pd.read_csv("HT_MD_polymer_properties.csv")
+    url = "https://huggingface.co/datasets/AdrianM0/bicerano_polymers/raw/main/HT_MD_polymer_properties.csv"
+    original_data = pd.read_csv(url)
     clean_data = original_data.drop("sl_num", axis=1)
 
     assert not clean_data.duplicated().sum()
@@ -30,8 +32,16 @@ def transform_data():
     clean_data["PSMILES"] = clean_data["PSMILES"].str.replace(
         "[Th]", "[*]", regex=False
     )
+    clean_data["PSMILES"] = clean_data["PSMILES"].str.replace(
+        "[[*]]", "[*]", regex=False
+    )
 
-    clean_data.to_csv("HT_MD_Polymers_clean.csv")
+    clean_data["PSMILES"] = clean_data["PSMILES"].apply(
+        lambda smiles: canonicalize(smiles)
+    )
+
+    clean_data.to_csv("data_clean.csv")
+
 
 
 if __name__ == "__main__":
