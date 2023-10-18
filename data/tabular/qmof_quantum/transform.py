bebf371b9d92f9b2547e4e99c0f7ee42c6f18bc3
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-10-19 00:17:55 +0200

    update MOF

---
+++ data/tabular/qmof_quantum/transform.py
@@ -0,0 +1,46 @@
+from datasets import load_dataset
+import ast
+
+
+def process():
+    dataset = load_dataset("kjappelbaum/chemnlp-qmof-data")["train"]
+    df = dataset.to_pandas()
+
+    df.dropna(
+        subset=[
+            "outputs.pbe.bandgap",
+            "outputs.pbe.cbm",
+            "outputs.pbe.vbm",
+            "outputs.hle17.bandgap",
+            "outputs.hle17.cbm",
+            "outputs.hle17.vbm",
+            "outputs.hse06.bandgap",
+            "outputs.hse06.cbm",
+            "outputs.hse06.vbm",
+            "info.pld",
+            "info.lcd",
+            "info.density",
+            "info.mofid.mofid",
+            "info.mofid.smiles_nodes",
+            "info.mofid.smiles_linkers",
+            "info.mofid.topology",
+            "info.symmetry.spacegroup_number",
+        ],
+        inplace=True,
+    )
+
+    df["info.mofid.smiles_nodes"] = df["info.mofid.smiles_nodes"].apply(
+        lambda x: ", ".join(ast.literal_eval(x))
+    )
+
+    df["info.mofid.smiles_linkers"] = df["info.mofid.smiles_linkers"].apply(
+        lambda x: ", ".join(ast.literal_eval(x))
+    )
+
+    print(len(df))
+
+    df.to_csv("data_clean.csv", index=False)
+
+
+if __name__ == "__main__":
+    process()
