bcbda9901bead5247e1cff9b9eb193c675104061
AdrianM0 <adrianmirza81@gmail.com>
2023-10-20 00:13:39 +0200

    feat: add MUV

---
+++ data/tabular/MUV_466/transform.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def transform_data():
+    df = pd.read_csv(
+        "https://huggingface.co/datasets/AdrianM0/MUV/raw/main/MUV_466/data_clean.csv"
+    )
+    df.to_csv("data_clean.csv", index=False)
+
+
+if __name__ == "__main__":
+    transform_data()
