6e9e2b6ed23846e343e67cbc96701e9072d2d89d
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-10-20 07:07:56 +0200

    update templates

--- data/tabular/smiles_to_3d/transform.py
+++ data/tabular/smiles_to_3d/transform.py
@@ -4,6 +4,7 @@ from datasets import load_dataset
 def process():
     dataset = load_dataset("kjappelbaum/chemnlp-qm9-file-translation")
     df = dataset["train"].to_pandas()
+    df.replace(to_replace="RDKit", value="ChemNLP", inplace=True)
     print(len(df))
     df.to_csv("data_clean.csv", index=False)
 
