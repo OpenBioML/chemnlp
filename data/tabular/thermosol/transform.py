e4841ef9af5ace9d723dba388df351b780f27609
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-10-24 09:00:13 +0200

    update solubility

--- data/tabular/thermosol/transform.py
+++ data/tabular/thermosol/transform.py
@@ -6,6 +6,8 @@ def process():
         "http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/thermosol.csv"
     )
     df.rename(columns={"smile": "SMILES"}, inplace=True)
+    df.dropna(inplace=True)
+    print(len(df))
     df[["SMILES", "target"]].to_csv("data_clean.csv", index=False)
 
 
