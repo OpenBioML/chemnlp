b32d7dcb69ab1eb4e82baf19cad3ae0cf1239a12
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-10-30 17:22:20 +0100

    update chemcaption

--- data/tabular/chemcaption_rdkit/transform.py
+++ data/tabular/chemcaption_rdkit/transform.py
@@ -1,14 +1,14 @@
-import pandas as pd 
-from huggingface_hub import list_repo_files
-from tqdm import tqdm
 import fire
-from datasets import load_dataset
+import pandas as pd
 
 
 def process():
-    df = pd.read_parquet('https://huggingface.co/datasets/kjappelbaum/chemnlp-chem-caption/resolve/main/rdkit_feat/train-00000-of-00001-27355e7935aa33a9.parquet')
+    df = pd.read_parquet(
+        "https://huggingface.co/datasets/kjappelbaum/chemnlp-chem-caption/resolve/main/rdkit_feat/train-00000-of-00001-27355e7935aa33a9.parquet"  # noqa
+    )
     print(df.columns)
-    df.to_csv('data_clean.csv', index=False)
+    df.to_csv("data_clean.csv", index=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     fire.Fire(process)
