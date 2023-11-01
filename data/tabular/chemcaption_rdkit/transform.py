9dad9953a4e4b43c299bee10e749dc49e7cf7190
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-11-01 17:35:58 +0100

    update

--- data/tabular/chemcaption_rdkit/transform.py
+++ data/tabular/chemcaption_rdkit/transform.py
@@ -6,10 +6,10 @@ def process():
     df = pd.read_parquet(
         "https://huggingface.co/datasets/kjappelbaum/chemnlp-chem-caption/resolve/main/rdkit_feat/train-00000-of-00001-27355e7935aa33a9.parquet"  # noqa
     )
-    df = df.query('num_single_bonds <= 1')
-    df = df.query('num_double_bonds <= 1')
-    df = df.query('num_triple_bonds <= 1')
-    df = df.query('num_aromatic_bonds <= 1')
+    df = df.query("num_single_bonds <= 1")
+    df = df.query("num_double_bonds <= 1")
+    df = df.query("num_triple_bonds <= 1")
+    df = df.query("num_aromatic_bonds <= 1")
     print(len(df))
     df.to_csv("data_clean.csv", index=False)
 
