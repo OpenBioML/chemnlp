f1c0a658e9851d334855a11e4402a4ba7b9c8ef0
Kevin M Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2023-11-01 17:36:16 +0100

    feat: add NER tasks (#460)
    Co-authored-by: Michael Pieler <36303596+MicPie@users.noreply.github.com>

---
+++ tests/test_ner.py
@@ -0,0 +1,70 @@
+from chemnlp.data.ner import group_tokens_by_labels, punctuation_joiner
+
+
+def test_tokens_by_label():
+    tokens = ["a", "b", "c", "d", "e", "f"]
+    labels = [0, 1, 1, 0, 1, 0]
+    grouped_tokens = group_tokens_by_labels(tokens, labels, join=False)
+    assert grouped_tokens == [["b"], ["c"], ["e"]]
+
+    labels = [0, 1, 2, 0, 1, 0]
+    grouped_tokens = group_tokens_by_labels(tokens, labels, join=False)
+    assert grouped_tokens == [["b", "c"], ["e"]]
+
+    labels = [0, 1, 1, 0, 1, 0]
+    grouped_tokens = group_tokens_by_labels(tokens, labels, join=True)
+    assert grouped_tokens == ["b", "c", "e"]
+
+    labels = [0, 1, 2, 0, 1, 0]
+    grouped_tokens = group_tokens_by_labels(tokens, labels, join=True)
+    assert grouped_tokens == ["b c", "e"]
+
+
+def test_join_punctuation():
+    token_list = [
+        "This",
+        "is",
+        "a",
+        "list",
+        "of",
+        "tokens",
+        "with",
+        "2",
+        ".",
+        "5",
+        ",",
+        "and",
+        "3",
+        "numbers",
+        "intact",
+        "semi",
+        "-",
+        "colon",
+        "separated",
+        "words",
+        "with",
+        "decimal",
+        "numbers",
+        "split",
+        "at",
+        "dots",
+        ".",
+        "This",
+        "is",
+        "a",
+        "comma",
+        ",",
+        "and",
+        "a",
+        "dot",
+        "(",
+        "test",
+        ")",
+        ".",
+    ]
+    sentence = punctuation_joiner(token_list)
+    print(sentence)
+    assert (
+        sentence
+        == "This is a list of tokens with 2.5, and 3 numbers intact semi-colon separated words with decimal numbers split at dots. This is a comma, and a dot (test)."  # noqa
+    )
