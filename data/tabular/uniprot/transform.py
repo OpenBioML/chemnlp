58639c58bdccbdfc229dbe8609f5694ccc027e11
AdrianM0 <adrianmirza81@gmail.com>
2023-11-01 22:29:06 +0100

    first version templates

---
+++ data/tabular/uniprot/transform.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+# https://huggingface.co/datasets/chemNLP/uniprot/resolve/main/reactions_sentences.csv
+def load_dataset() -> pd.DataFrame:
+    uniprot = pd.read_csv("reactions_sentences.csv")
+    uniprot.to_csv("data_clean.csv", index=False)
+    return uniprot
+
+
+if __name__ == "__main__":
+    
+    print(len(load_dataset()))
+    
+    
