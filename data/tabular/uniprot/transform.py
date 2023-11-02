388e822d807affaf6baf355bafee5780c95a843c
AdrianM0 <adrianmirza81@gmail.com>
2023-11-02 14:25:11 +0100

    pre-commit

--- data/tabular/uniprot/transform.py
+++ data/tabular/uniprot/transform.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def load_dataset() -> pd.DataFrame:
     uniprot = pd.read_csv(
-        "https://huggingface.co/datasets/chemNLP/uniprot/resolve/main/reactions_sentences_domains_organisms_binding_sites.csv" # noqa
+        "https://huggingface.co/datasets/chemNLP/uniprot/resolve/main/reactions_sentences_domains_organisms_binding_sites.csv"  # noqa: E501
     )
     uniprot.rename(columns={"sequence": "other"}, inplace=True)
     uniprot.to_csv("data_clean.csv", index=False)
