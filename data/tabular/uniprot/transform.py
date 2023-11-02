ed6b5322f1b91e33460812828094e2faa3a7bf87
AdrianM0 <adrianmirza81@gmail.com>
2023-11-02 14:21:33 +0100

    lint

--- data/tabular/uniprot/transform.py
+++ data/tabular/uniprot/transform.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 
 def load_dataset() -> pd.DataFrame:
-    uniprot = pd.read_csv("https://huggingface.co/datasets/chemNLP/uniprot/resolve/main/reactions_sentences_domains_organisms_binding_sites.csv")
+    uniprot = pd.read_csv(
+        "https://huggingface.co/datasets/chemNLP/uniprot/resolve/main/reactions_sentences_domains_organisms_binding_sites.csv" # noqa
+    )
     uniprot.rename(columns={"sequence": "other"}, inplace=True)
     uniprot.to_csv("data_clean.csv", index=False)
     print("Successfully loaded dataset!")
