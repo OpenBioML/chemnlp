83747a5590f2536681491b15dddb881c18d88082
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-11-07 00:20:31 +0100

    update

--- data/tabular/qmof_gcmc/transform.py
+++ data/tabular/qmof_gcmc/transform.py
@@ -62,11 +62,11 @@ def process():
     df["lg10_CO2_Henry"] = np.log10(df["outputs.CO2-henry_coefficient-mol--kg--Pa"])
     df["lg10_N2_Henry"] = np.log10(df["outputs.N2-henry_coefficient-mol--kg--Pa"])
     df["lg10_CH4_Henry"] = np.log10(df["outputs.CH4-henry_coefficient-mol--kg--Pa"])
-    df["log10_O2_Henry"] = np.log10(df["outputs.O2-henry_coefficient-mol--kg--Pa"])
-    df["log10_Xe_Henry"] = np.log10(df["outputs.Xe-henry_coefficient-mol--kg--Pa"])
-    df["log10_Kr_Henry"] = np.log10(df["outputs.Kr-henry_coefficient-mol--kg--Pa"])
-    df["log10_H2S_Henry"] = np.log(df["outputs.H2S-henry_coefficient-mol--kg--Pa"])
-    df["log10_H20_Henry"] = np.log(df["outputs.H2O-henry_coefficient-mol--kg--Pa"])
+    df["lg10_O2_Henry"] = np.log10(df["outputs.O2-henry_coefficient-mol--kg--Pa"])
+    df["lg10_Xe_Henry"] = np.log10(df["outputs.Xe-henry_coefficient-mol--kg--Pa"])
+    df["lg10_Kr_Henry"] = np.log10(df["outputs.Kr-henry_coefficient-mol--kg--Pa"])
+    df["lg10_H2S_Henry"] = np.log(df["outputs.H2S-henry_coefficient-mol--kg--Pa"])
+    df["lg10_H20_Henry"] = np.log(df["outputs.H2O-henry_coefficient-mol--kg--Pa"])
 
     df["info.mofid.smiles_nodes"] = df["info.mofid.smiles_nodes"].apply(
         lambda x: ", ".join(ast.literal_eval(x))
