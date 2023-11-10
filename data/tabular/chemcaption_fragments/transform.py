d31fa15036363559827cbb8f558e1a39e0a5d22a
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2023-11-10 23:54:30 +0100

    lint

--- data/tabular/chemcaption_fragments/transform.py
+++ data/tabular/chemcaption_fragments/transform.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def process():
     df = pd.read_parquet(
-        "https://huggingface.co/datasets/kjappelbaum/chemnlp-chem-caption/resolve/main/smarts/train-00000-of-00001-71cef18c6383b463.parquet?download=true"
+        "https://huggingface.co/datasets/kjappelbaum/chemnlp-chem-caption/resolve/main/smarts/train-00000-of-00001-71cef18c6383b463.parquet?download=true"  # noqa
     )
     df.dropna(inplace=True)
     print(len(df))
