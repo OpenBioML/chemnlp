90a1ebd8d76137e9f71c158c0dc2e535e27cb4cd
Michael Pieler <Michael.Pieler@Gmail.com>
2024-02-05 17:43:09 +0100

    additional fixes 2

--- data/tabular/solubility_aqsoldb/transform.py
+++ data/tabular/solubility_aqsoldb/transform.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import yaml
+
+# import yaml
 from tdc.single_pred import ADME
 
 
@@ -43,102 +44,102 @@ def get_and_transform_data():
     # save to csv
     fn_data_csv = "data_clean.csv"
     df.to_csv(fn_data_csv, index=False)
-
+    # META YAML IS NOT USED!
     # create meta yaml
-    meta = {
-        "name": "solubility_aqsoldb",  # unique identifier, we will also use this for directory names
-        "description": """Aqeuous solubility measures a drug's ability to dissolve in water.
-Poor water solubility could lead to slow drug absorptions, inadequate bioavailablity
-and even induce toxicity. More than 40 percent of new chemical entities are
-not soluble.""",
-        "targets": [
-            {
-                "id": "aqeuous_solubility",  # name of the column in a tabular dataset
-                "description": "aqueous solubility",  # description of what this column means
-                "units": "log(mol/L)",  # units of the values in this column (leave empty if unitless)
-                "type": "continuous",
-                "names": [  # names for the property (to sample from for building the prompts)
-                    {"noun": "aqeuous solubility"},
-                    {"noun": "water solubility"},
-                    {"noun": "ability of a drug to dissolve in a water"},
-                    {"adjective": "dissolves in a water"},
-                ],
-                "uris": [
-                    "http://purl.jp/bio/4/id/200906006880450101",
-                    "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C60821",
-                ],
-            },
-        ],
-        "benchmarks": [
-            {
-                "name": "TDC",  # unique benchmark name
-                "link": "https://tdcommons.ai/",  # benchmark URL
-                "split_column": "split",  # name of the column that contains the split information
-            },
-        ],
-        "identifiers": [
-            {
-                "id": "SMILES",  # column name
-                "type": "SMILES",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
-                "description": "SMILES",  # description (optional, except for "Other")
-            },
-            {
-                "id": "compound_name",  # column name
-                "type": "Other",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
-                "names": [
-                    {"noun": "compound name"},
-                    {"noun": "drug name"},
-                    {"noun": "generic drug name"},
-                ],
-                "description": "compound name",  # description (optional, except for "Other")
-            },
-        ],
-        "license": "CC BY 4.0",  # license under which the original dataset was published
-        "links": [  # list of relevant links (original dataset, other uses, etc.)
-            {
-                "url": "https://doi.org/10.1038/s41597-019-0151-1",
-                "description": "corresponding publication",
-            },
-            {
-                "url": "https://tdcommons.ai/single_pred_tasks/adme/#solubility-aqsoldb",
-                "description": "data source",
-            },
-        ],
-        "num_points": len(df),  # number of datapoints in this dataset
-        "bibtex": [
-            """@article{Sorkun_2019,
-doi = {10.1038/s41597-019-0151-1},
-url = {https://doi.org/10.1038%2Fs41597-019-0151-1},
-year = {2019},
-month = aug,
-publisher = {Springer Science and Business Media LLC},
-volume = {6},
-number = {1},
-author = {Murat Cihan Sorkun and Abhishek Khetan and
-Suleyman Er},
-title = {AqSolDB, a curated reference set of aqueous solubility
-and 2D descriptors for a diverse set of compounds},
-journal = {Scientific Data}""",
-        ],
-    }
+    #    meta = {
+    #        "name": "solubility_aqsoldb",  # unique identifier, we will also use this for directory names
+    #        "description": """Aqeuous solubility measures a drug's ability to dissolve in water.
+    # Poor water solubility could lead to slow drug absorptions, inadequate bioavailablity
+    # and even induce toxicity. More than 40 percent of new chemical entities are
+    # not soluble.""",
+    #        "targets": [
+    #            {
+    #                "id": "aqeuous_solubility",  # name of the column in a tabular dataset
+    #                "description": "aqueous solubility",  # description of what this column means
+    #                "units": "log(mol/L)",  # units of the values in this column (leave empty if unitless)
+    #                "type": "continuous",
+    #                "names": [  # names for the property (to sample from for building the prompts)
+    #                    {"noun": "aqeuous solubility"},
+    #                    {"noun": "water solubility"},
+    #                    {"noun": "ability of a drug to dissolve in a water"},
+    #                    {"adjective": "dissolves in a water"},
+    #                ],
+    #                "uris": [
+    #                    "http://purl.jp/bio/4/id/200906006880450101",
+    #                    "http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#C60821",
+    #                ],
+    #            },
+    #        ],
+    #        "benchmarks": [
+    #            {
+    #                "name": "TDC",  # unique benchmark name
+    #                "link": "https://tdcommons.ai/",  # benchmark URL
+    #                "split_column": "split",  # name of the column that contains the split information
+    #            },
+    #        ],
+    #        "identifiers": [
+    #            {
+    #                "id": "SMILES",  # column name
+    #                "type": "SMILES",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
+    #                "description": "SMILES",  # description (optional, except for "Other")
+    #            },
+    #            {
+    #                "id": "compound_name",  # column name
+    #                "type": "Other",  # can be "SMILES", "SELFIES", "IUPAC", "Other"
+    #                "names": [
+    #                    {"noun": "compound name"},
+    #                    {"noun": "drug name"},
+    #                    {"noun": "generic drug name"},
+    #                ],
+    #                "description": "compound name",  # description (optional, except for "Other")
+    #            },
+    #        ],
+    #        "license": "CC BY 4.0",  # license under which the original dataset was published
+    #        "links": [  # list of relevant links (original dataset, other uses, etc.)
+    #            {
+    #                "url": "https://doi.org/10.1038/s41597-019-0151-1",
+    #                "description": "corresponding publication",
+    #            },
+    #            {
+    #                "url": "https://tdcommons.ai/single_pred_tasks/adme/#solubility-aqsoldb",
+    #                "description": "data source",
+    #            },
+    #        ],
+    #        "num_points": len(df),  # number of datapoints in this dataset
+    #        "bibtex": [
+    #            """@article{Sorkun_2019,
+    # doi = {10.1038/s41597-019-0151-1},
+    # url = {https://doi.org/10.1038%2Fs41597-019-0151-1},
+    # year = {2019},
+    # month = aug,
+    # publisher = {Springer Science and Business Media LLC},
+    # volume = {6},
+    # number = {1},
+    # author = {Murat Cihan Sorkun and Abhishek Khetan and
+    # Suleyman Er},
+    # title = {AqSolDB, a curated reference set of aqueous solubility
+    # and 2D descriptors for a diverse set of compounds},
+    # journal = {Scientific Data}""",
+    #        ],
+    #    }
+    #
+    #    def str_presenter(dumper, data):
+    #        """configures yaml for dumping multiline strings
+    #        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
+    #        """
+    #        if data.count("\n") > 0:  # check for multiline string
+    #            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+    #        return dumper.represent_scalar("tag:yaml.org,2002:str", data)
+    #
+    #    yaml.add_representer(str, str_presenter)
+    #    yaml.representer.SafeRepresenter.add_representer(
+    #        str, str_presenter
+    #    )  # to use with safe_dum
+    #    fn_meta = "meta.yaml"
+    #    with open(fn_meta, "w") as f:
+    #        yaml.dump(meta, f, sort_keys=False)
 
-    def str_presenter(dumper, data):
-        """configures yaml for dumping multiline strings
-        Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data
-        """
-        if data.count("\n") > 0:  # check for multiline string
-            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
-        return dumper.represent_scalar("tag:yaml.org,2002:str", data)
-
-    yaml.add_representer(str, str_presenter)
-    yaml.representer.SafeRepresenter.add_representer(
-        str, str_presenter
-    )  # to use with safe_dum
-    fn_meta = "meta.yaml"
-    with open(fn_meta, "w") as f:
-        yaml.dump(meta, f, sort_keys=False)
-
-    print(f"Finished processing {meta['name']} dataset!")
+    # print(f"Finished processing {meta['name']} dataset!")
 
 
 if __name__ == "__main__":
