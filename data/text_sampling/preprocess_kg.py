ac9c8c0cd84936aceb6130264131fb6a18b5c391
Michael Pieler <36303596+MicPie@users.noreply.github.com>
2024-02-06 15:28:09 +0100

    Update data/text_sampling/preprocess_kg.py

--- data/text_sampling/preprocess_kg.py
+++ data/text_sampling/preprocess_kg.py
@@ -76,7 +76,7 @@ Assistant: The {node2_type#} {node2_protein_names#} {rel2_type#} a {node3_name#}
     # and this is not handled yet properly.
     #    "compound_protein_go_term": [
     #        """The {node1_type#} {SMILES__description} {SMILES#} {rel1_type#} the {node2_type#} {node2_protein_names#} which {rel2_type#} the {node3_name#}.""",  # noqa E501
-    #        """The {node1_type#} {SMILES#} {rel1_type#} the {node2_type#} {node2_protein_names#}. The {node2_type#} {node2_protein_names#} {rel2_type#} the {node3_name#}.""",  # noqa E501
+    #        """The {node1_type#} {SMILES__description} {SMILES#} {rel1_type#} the {node2_type#} {node2_protein_names#}. The {node2_type#} {node2_protein_names#} {rel2_type#} the {node3_name#}.""",  # noqa E501
     # ],
     "compound_protein_hpo": [
         """The {node1_type#} {SMILES#} {rel1_type#} the {node2_type#} {node2_protein_names#} and {rel2_type#} {node3_name#}.""",  # noqa E501
