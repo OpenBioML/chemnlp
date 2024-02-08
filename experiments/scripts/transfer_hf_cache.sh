cd90d39c5760bf28e7b8fa2deec9f98154b96ceb
Michael Pieler <Michael.Pieler@Gmail.com>
2024-02-08 17:22:04 +0100

    feat: add data/tabular/bicerano_dataset/meta.yaml

--- experiments/scripts/transfer_hf_cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-hf_subdirs=( accelerate datasets hub )
-for sub_dir in "${hf_subdirs[@]}"
-do
-   cp -R ~/.cache/huggingface/$sub_dir/* /fsx/proj-chemnlp/hf_cache/$sub_dir
-   rm -rf ~/.cache/huggingface/$sub_dir
-done
